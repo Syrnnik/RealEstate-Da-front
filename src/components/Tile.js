b2581553453cefc5ffdd3977bc7985e857a9c54f
Syrnnik <sakh.alexandr@gmail.com>
2023-03-31 20:14:46 +0300

    fix tiles navigation on main page

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {NavLink, Link} from 'react-router-dom';
-import {animateScroll as scroll} from 'react-scroll';
-
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Tile(props) {
-
     const [mob, setMob] = useState(false);
     const [visibility, setVisibility] = useState(false);
 
@@ -17,9 +14,9 @@ export default function Tile(props) {
             }
         }
 
-        window.addEventListener('resize', changeTile);
+        window.addEventListener("resize", changeTile);
         changeTile();
-        return () => window.removeEventListener('resize', changeTile);
+        return () => window.removeEventListener("resize", changeTile);
     }, []);
 
     const titles = props.titles;
@@ -28,99 +25,77 @@ export default function Tile(props) {
     if (mob) {
         return (
             <div className="tile" onClick={() => setVisibility(true)}>
-                <img src={props.img} alt="иконка"/>
+                <img src={props.img} alt="иконка" />
                 <div className="links">
-                    {
-                        (props.simpleLink) &&
-                        <Link
-                            className="title"
-                            to={props.simpleLink.url}
-                        >
+                    {props.simpleLink && (
+                        <Link className="title" to={props.simpleLink.url}>
                             {props.simpleLink.title}
                         </Link>
-                    }
-                    {
-                        (titles) &&
+                    )}
+                    {titles &&
                         titles.map(function (item) {
                             return (
-                                <div key={item} className="title">{item}</div>
-                            )
-                        })
-                    }
+                                <div key={item} className="title">
+                                    {item}
+                                </div>
+                            );
+                        })}
                 </div>
-                {
-                    (hoverLinks) &&
-                    <div className={(visibility) ? "click-links" : "click-links d-none"}>
+                {hoverLinks && (
+                    <div className={visibility ? "click-links" : "click-links d-none"}>
                         <div className="titles mb-2 mb-sm-3">
-                            {
-                                titles.map(function (item) {
-                                    return (
-                                        <div key={item} className="title">{item}</div>
-                                    )
-                                })
-                            }
+                            {titles.map(function (item) {
+                                return (
+                                    <div key={item} className="title">
+                                        {item}
+                                    </div>
+                                );
+                            })}
                         </div>
                         <ul>
-                            {
-                                hoverLinks.map(function (item, i) {
-                                    return (
-                                        <li key={i}>
-                                            <NavLink
-                                                to={item.link}
-                                            >
-                                                {item.name}
-                                            </NavLink>
-                                        </li>
-                                    )
-                                })
-                            }
+                            {hoverLinks.map(function (item, i) {
+                                return (
+                                    <li key={i}>
+                                        <NavLink to={item.link}>{item.name}</NavLink>
+                                    </li>
+                                );
+                            })}
                         </ul>
                     </div>
-                }
+                )}
             </div>
-        )
+        );
     } else {
         return (
             <div className="tile">
-                <img src={props.img} alt="иконка"/>
+                <img src={props.img} alt="иконка" />
                 <div className="links">
-                    {
-                        (props.simpleLink) &&
-                        <Link
-                            className="title"
-                            to={props.simpleLink.url}
-                        >
+                    {props.simpleLink && (
+                        <Link className="title" to={props.simpleLink.url}>
                             {props.simpleLink.title}
                         </Link>
-                    }
-                    {
-                        (titles) &&
+                    )}
+                    {titles &&
                         titles.map(function (item) {
                             return (
-                                <div key={item} className="title">{item}</div>
-                            )
-                        })
-                    }
-                    {
-                        (hoverLinks) &&
+                                <div key={item} className="title">
+                                    {item}
+                                </div>
+                            );
+                        })}
+                    {hoverLinks && (
                         <div className="hover-links">
-                            {
-                                hoverLinks.map(function (item) {
-                                    return (
-                                        <div key={item.name}>
-                                            <NavLink
-                                                to={item.link}
-                                            >
-                                                {item.name}
-                                            </NavLink>
-                                        </div>
-                                    )
-                                })
-                            }
+                            {hoverLinks.map(function (item) {
+                                return (
+                                    <div key={item.name}>
+                                        <NavLink to={item.link}>{item.name}</NavLink>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    }
+                    )}
                 </div>
             </div>
-        )
+        );
     }
 }
--- src/components/TileServices.js
+++ src/components/TileServices.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const TileServices = (props) => {
     return (
         <div className="tile">
-            <img src={props.img}/>
-            <div className="links">
-                {
-                    props.dynamic
-                        ?
-                        <NavLink to={`service/${props.slug}`} className='title services-links'>
+            <NavLink to={`service/${props.slug}`}>
+                <img src={props.img} />
+                <div className="links">
+                    {props.dynamic ? (
+                        <div className="title services-links">{props.name}</div>
+                    ) : (
+                        <NavLink to={`hypothec`} className="title services-links">
                             {props.name}
                         </NavLink>
-                        :
-                        <NavLink to={`hypothec`} className='title services-links'>
-                            {props.name}
-                        </NavLink>
-                }
-            </div>
+                    )}
+                </div>
+            </NavLink>
         </div>
     );
 };
